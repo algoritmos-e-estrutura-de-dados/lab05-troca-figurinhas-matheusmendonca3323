bde01d3c734d27ee3c236e94aa3b510622f194e4
matheusmendonca3323 <110430944+matheusmendonca3323@users.noreply.github.com>
2022-10-02 22:37:34 -0300

    Update main.py
    correção do repositório

--- labs/atividade-05/main.py
+++ labs/atividade-05/main.py
@@ -1,12 +1,29 @@
 def maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_do_joao):
-    ''' Implementar a solucao nesta função
-    '''
-    return 0
+  
+    z = 0
+    qtd = 0
+
+    if len(figurinhas_da_maria) < len(figurinhas_do_joao):  
+        for i in range(len(figurinhas_da_maria)):
+            for j in range(len(figurinhas_do_joao)):
+                if figurinhas_da_maria[i] == figurinhas_do_joao[j]:
+                    z += 1
+        qtd = len(figurinhas_da_maria) - z
 
 
+    else if len(figurinhas_do_joao) < len(figurinhas_da_maria):
+        for i in range(len(figurinhas_do_joao)):
+            for j in range(len(figurinhas_da_maria)):
+                if figurinhas_do_joao[i] == figurinhas_da_maria[j]:
+                    z += 1
+        qtd = len(figurinhas_do_joao) - z
 
-if __name__ == '__main__':
-    A, B = input().split(' ')
-    figurinhas_da_maria = input().split(' ')
-    figurinhas_da_joao = input().split(' ')
-    maximizar_troca_de_figurinhas(figurinhas_da_maria, figurinhas_da_joao)
+                                                              
+    else:
+        for i in range(len(figurinhas_da_maria)):
+            for j in range(len(figurinhas_do_joao)):
+                if figurinhas_da_maria[i] == figurinhas_do_joao[j]:
+                    z += 1
+        qtd = len(figurinhas_da_maria) - z
+
+    return qtd
